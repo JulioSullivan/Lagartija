2afa6f9eb0347a532cd879f93039a38ce9aff0dc
Julio Sánchez <juliosullivano@gmail.com>
2018-10-09 23:45:38 -0500

    Primer commit del proyecto del genom

--- prueba-lectura.py
+++ prueba-lectura.py
@@ -1,7 +1,8 @@
 import itertools
 import pdb
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import NMF
+from sklearn.decomposition import TruncatedSVD
+import re
 
 scaffolds = []
 with open('prueba.fa') as f, open("out.fa", "w") as o:
@@ -53,18 +54,34 @@ def display_topics(model, feature_names, no_top_words):
     for topic_idx, topic in enumerate(model.components_):
         print("Topic %d:" % (topic_idx))
         print(" ".join([feature_names[i]
-                        for i in topic.argsort()[:-no_top_words - 1:-1]]))
+            for i in topic.argsort()[:-no_top_words - 1:-1]]))
 
 
 it = Iterador('out.fa')
 
 
-vectorizer = TfidfVectorizer(decode_error='replace', analyzer='char', ngram_range=(3,10))
+vectorizer = TfidfVectorizer(decode_error='replace', analyzer='char', ngram_range=(3,10), lowercase='false')
 
-#pdb.set_trace()
+#x_vectorizer = vectorizer.fit_transform(it)
+x_vectorizer = vectorizer.fit(it)
+voc = vectorizer.vocabulary_
+vocList = vectorizer.vocabulary_.keys()
+
+r = re.compile("^n*$")
+ns = list(filter(r.match, vocList))
+
+for n in ns:
+    del vectorizer.vocabulary_[n]
+
+if "nnn" in vocList:
+    print("hola")
+
+vectorizer = TfidfVectorizer(vocabulary=vectorizer.vocabulary_.keys())
 x_vectorizer = vectorizer.fit_transform(it)
 no_topics = 3
-nmf = NMF(n_components=no_topics, random_state=1, alpha=0.1, l1_ratio=.5, init='nndsvd').fit(x_vectorizer)
+nmf = TruncatedSVD(n_components=no_topics, random_state=1).fit(x_vectorizer)
+print(x_vectorizer.shape)
+print(len(vectorizer.vocabulary_))
 
 tfidf_feature_names = vectorizer.get_feature_names()
 no_top_words = 10
