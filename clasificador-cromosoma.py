242c57fbb47bc9496fdef89e18d9ecb47dffea13
Julio Sánchez <juliosullivano@gmail.com>
2018-11-13 00:55:18 -0600

    Se agrega la SVM

--- clasificador-cromosoma.py
+++ clasificador-cromosoma.py
@@ -4,7 +4,12 @@ from sklearn.metrics import classification_report
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
 from operator import itemgetter
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_selection import SelectKBest, chi2
 import re
+import numpy as np
+import matplotlib.pyplot as plt
 import pdb
 import random
 
@@ -39,15 +44,20 @@ vect = []
 Y = []
 X = []
 
-inputfa = '/home/juliosullivan/Documents/joinP1.fa'
+inputfa = 'joinP1.fa'
 
 with open(inputfa) as file:
     for line in file:
         Y.append(line.split('\t')[0])
-
+"""
 cromosoma = Pipeline([('tfidf', TfidfVectorizer(decode_error='replace', analyzer='char', ngram_range=(3,5), lowercase=False)),
     ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5, tol=None, n_jobs=-1)),
 ])
+"""
+cromosoma = Pipeline([('tfidf', TfidfVectorizer(decode_error='replace', analyzer='char', ngram_range=(3,5), lowercase=False)),
+    ('clf', svm.SVC(gamma='auto', random_state=42, max_iter=5)),
+])
+
 
 
 indices = list(range(len(Y)))
@@ -74,3 +84,45 @@ predicted = cromosoma.predict(it_test)
 print(getter_test(Y))
 print(predicted)
 print(classification_report(getter_test(Y), predicted))
+
+N_FEATURES_OPTIONS = ['Scaffold']
+C_OPTIONS = [0, 1]
+
+param_grid = [
+    {
+        'reduce_dim': [TfidfVectorizer(decode_error='replace', analyzer='char', ngram_range=(3,5), lowercase=False)],
+        'reduce_dim__n_components': N_FEATURES_OPTIONS,
+        'classify__C': C_OPTIONS
+    },
+    {
+        'reduce_dim': [SelectKBest(chi2)],
+        'reduce_dim__k': N_FEATURES_OPTIONS,
+        'classify__C': C_OPTIONS
+    },
+]
+reducer_labels = ['TfidfVectorizer', 'KBest(chi2)']
+
+grid = GridSearchCV(cromosoma, cv=5,  n_jobs=-1, param_grid=param_grid)
+grid.fit(list(it), getter_train(Y))
+
+mean_scores = np.array(grid.cv_results_['mean_test_score'])
+# scores are in the order of param_grid iteration, which is alphabetical
+mean_scores = mean_scores.reshape(len(C_OPTIONS), 1, len(N_FEATURES_OPTIONS))
+# select score for best C
+mean_scores = mean_scores.max(axis=0)
+bar_offsets = (np.arange(len(N_FEATURES_OPTIONS)) *
+               (len(reducer_labels) + 1) + .5)
+
+plt.figure()
+COLORS = 'bgrcmyk'
+for i, (label, reducer_scores) in enumerate(zip(reducer_labels, mean_scores)):
+    plt.bar(bar_offsets + i, reducer_scores, label=label, color=COLORS[i])
+
+plt.title("Comparing feature reduction techniques")
+plt.xlabel('Reduced number of features')
+plt.xticks(bar_offsets + len(reducer_labels) / 2, N_FEATURES_OPTIONS)
+plt.ylabel('Digit classification accuracy')
+plt.ylim((0, 1))
+plt.legend(loc='upper left')
+
+plt.show()
