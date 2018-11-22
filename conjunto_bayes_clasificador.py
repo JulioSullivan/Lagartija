b23a724c4b5bc90d29021a527be55c471c76ff92
Julio Sánchez <juliosullivan@comunidad.unam.mx>
2018-11-22 11:39:44 -0600

    SE agrega archivo para los 10 JOIN con bayes

--- conjunto_bayes_clasificador.py
+++ conjunto_bayes_clasificador.py
@@ -41,29 +41,24 @@ class Iterador(object):
                     yield re.sub('N','',line.split("\t")[2])
 
 cromosoma = Pipeline([
-    ('tfidf', TfidfVectorizer(decode_error='replace', 
-                analyzer='char', ngram_range=(3,5), 
-                lowercase=False)),
-    ('clf', svm.SVC(gamma='auto', random_state=42, C=50, 
-                    decision_function_shape='ovo', kernel='linear', degree=1)),
+    ('countV', CountVectorizer(analyzer='char', lowercase=False)), 
+    ('clf', MultinomialNB())
 ])
 
 parameters = {
-    'tfidf__ngram_range':((2, 3), (2, 4), (2, 5), (3,4), (3, 5)),
-    #'clf__degree': (1,2),
-    'clf__C': (500, 1000, 3000, 5000),
+    'countV__ngram_range': ((2, 3), (2, 4), (2, 5), (3, 4), (3,5)),
 }
 
 
-for i in range(4, 11):
+for i in range(2, 11):
     vect = []
     Y = []
     X = []
 
     inputfa = 'joinP'+str(i)+'.fa'
-    out_model = 'trained_model_joinP'+str(i)+'.pkl'
-    out_tfidf = 'tfidf_model_joinP'+str(i)+'.pkl'
-    resultados_GRID = 'resultadosGRID_joinP'+str(i)+'.txt'
+    out_model = 'trained_model_joinP'+str(i)+'_bayes.pkl'
+    out_tfidf = 'countV_model_joinP'+str(i)+'_bayes.pkl'
+    resultados_GRID = 'resultadosGRID_joinP'+str(i)+'_bayes.txt'
 
     with open(inputfa) as file:
         for line in file:
@@ -98,17 +93,16 @@ for i in range(4, 11):
     it_test = Iterador(inputfa, in_test)
 
     predicted = grid.predict(it_test)
+    # Luego, guardamos el modelo
+    joblib.dump(grid.best_estimator_.named_steps['clf'], out_model, compress = 1)
+    joblib.dump(grid.best_estimator_.named_steps['countV'], out_tfidf, compress = 1)
+
     resGRID.write("\nClassification report:\n")
     resGRID.write(classification_report(getter_test(Y), predicted))
 
     resGRID.write("\nModel properties:\n")
     #grid.best_estimator_.named_steps['clf'].support_
-    resGRID.write("Support size: %s" % np.array2string(grid.best_estimator_.named_steps['clf'].n_support_))
-    resGRID.write("Support indices: %s" % np.array2string(grid.best_estimator_.named_steps['clf'].support_))
+    resGRID.write("Matriz de Probabilidades:  %s" % np.array2string(grid.best_estimator_.named_steps['clf'].feature_log_prob_))
     # Y así imprime los parametros del modelo en el archivo de resultados
     # ...
     resGRID.close()
-
-    # Luego, guardamos el modelo
-    joblib.dump(grid.best_estimator_.named_steps['clf'], out_model, compress = 1)
-    joblib.dump(grid.best_estimator_.named_steps['tfidf'], out_tfidf, compress = 1)
