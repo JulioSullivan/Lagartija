308eb02321151b48bfb17c9e0d6d60ad6e897bd9
Ignacio Arroyo Fernandez <iarroyof@compute-0-3.local>
2018-11-22 11:11:34 -0600

    Pruebas con bayes de los 10 archivos

--- conjunto_bayes_clasificador.py
+++ conjunto_bayes_clasificador.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 from operator import itemgetter
 from sklearn import svm
 from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 import re
 import numpy as np
 from pdb import set_trace as st
