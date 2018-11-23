e0f7cb0224722e3092bb6926401969db254ab9cb
Julio Sánchez <juliosullivano@gmail.com>
2018-11-23 02:13:57 -0600

    Se agrega archivo para leer los pesos

--- lector-modelo.py
+++ lector-modelo.py
@@ -1,5 +1,21 @@
 from sklearn.externals import joblib
 import pdb
-loaded_model = joblib.load('trained_model_02.pkl')
-loaded_idf = joblib.load('idf_model.pkl')
-pdb.set_trace()
+
+loaded_model = joblib.load('trained_model_joinP10.pkl')
+loaded_idf = joblib.load('tfidf_model_joinP10.pkl')
+
+lista_aux = []
+indice_peso = []
+pesos = loaded_model.coef_
+vocab = loaded_idf.vocabulary_
+
+for i in range(pesos.shape[1]):
+    indice_peso.append((pesos[0, i], i))
+    
+indice_peso.sort(key=lambda tup: tup[0], reverse = True)
+
+for i in range(10):
+    print(indice_peso[i])
+    for n, seq in enumerate(vocab):
+        if n == indice_peso[i][1]:   
+            print(seq)
